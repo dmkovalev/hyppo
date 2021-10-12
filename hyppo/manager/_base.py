df63b57b67b4a9d17626e8c18d779d56791a981d
Dmitry Kovalev <dm.kovalev@gmail.com>
2021-10-12 22:43:24 +0300

    add properties to manager

--- hyppo/manager/_base.py
+++ hyppo/manager/_base.py
@@ -4,21 +4,25 @@ from hyppo.core._base import virtual_experiment_onto
 manager_onto = get_ontology("https://synthesis.ipi.ac.ru/manager.owl")
 
 with manager_onto:
-    class Manager(Thing): pass
-    class has_for_experiment(Manager >> virtual_experiment_onto.VirtualExperiment): pass
-    class has_for_execution_plan(Manager >> ExecutionPlan): pass
-    class has_for_lattice(Manager >> HypothesisLattice): pass
-    class has_for_runner(Manager >> Runner): pass
-    class has_for_domain_ontology(Manager >> Ontology): pass
+    class Manager(virtual_experiment_onto.Artefact):
+        def run(self):
+            pass
+
+    class has_for_virtual_experiment(Manager >> virtual_experiment_onto.VirtualExperiment):
+        python_name = 'virtual_experiment'
+
+    class has_for_execution_plan(Manager >> ExecutionPlan):
+        python_name = 'execution_plan'
+
+    class has_for_lattice(Manager >> HypothesisLattice):
+        python_name = 'hypothesis_lattice'
+
+    class has_for_runner(Manager >> Runner):
+        python_name = 'runner'
+
+    class has_for_domain_ontology(Manager >> Ontology):
+        python_name = 'domain_ontology'
+
     class has_for_autogenerate(Manager >> bool, DataProperty, FunctionalProperty): pass
-
-
-
-def delete(ve, id, type):
-    ve.delete(type, id)
-    return
-
-
-def modify(ve, type, id, specification):
-    ve.modify(type, id, specification)
-    return
+    class has_for_abandon_rules(Manager >> Rule): pass
+    class has_for_correlation(Manager >> Correlation): pass
