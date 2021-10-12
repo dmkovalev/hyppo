8d4c01d0ddc4f6dc925024d308a84da784a3ef03
dmitry.kovalev <dmitry.kovalev@aramcoinnovations.com>
2021-10-12 11:41:55 +0300

    add image details

--- hyppo/core/_hcp_brain.py
+++ hyppo/core/_hcp_brain.py
@@ -4,21 +4,28 @@ hcp_brain_onto = get_ontology("http://synthesis.ipi.ac.ru/hcp_onto.owl")
 
 with hcp_brain_onto:
     class Human(Thing): pass
-    class Image(Thing): pass
     class Brain(Thing): pass
+    class Image(Thing): pass
+    class ROI(Thing): pass
+    class Voxel(Thing): pass
 
     class has_for_age_from(Human >> int, DataProperty, FunctionalProperty): pass
     class has_for_age_to(Human >> int, DataProperty, FunctionalProperty): pass
 
-    class has_for_gender(Human >> str): pass
+    class has_for_gender(Human >> str, DataProperty, FunctionalProperty): pass
     class has_for_brain(Human >> Brain): pass
     class has_for_image(Brain >> Image): pass
+    class has_for_roi(Image >> ROI): pass
+    class has_for_voxel(Image >> Voxel): pass
 
-    class YoungAdult(Human):
-        equivalent_to = [Human & has_for_age_from > 20 & has_for_age_to < 30]
+    class Adult(Human):
+        equivalent_to = [Human & has_for_age_from > 18]
 
-    class MiddleAgedAdult(Human):
-        equivalent_to = [Human & has_for_age_from > 30 & has_for_age_to < 40]
+    class YoungAdult(Adult):
+        equivalent_to = [Adult & has_for_age_to < 30]
+
+    class MiddleAgedAdult(Adult):
+        equivalent_to = [Adult & has_for_age_from > 30 & has_for_age_to < 40]
 
     class Man(Human):
         equivalent_to = [Human & has_for_gender == 'male']
