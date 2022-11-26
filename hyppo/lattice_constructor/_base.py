50f1c1cf31044595607910d0d6b83e8635cd1e4d
dmitry.kovalev <dm.kovalev@gmail.com>
2022-11-26 21:41:49 +0300

    add add hypothesis to lattice pseudo code

--- hyppo/lattice_constructor/_base.py
+++ hyppo/lattice_constructor/_base.py
@@ -1,6 +1,8 @@
 from hyppo.core._base import virtual_experiment_onto
 from hyppo.coa._base import Structure, Equation
 import networkx as nx
+from collections import defaultdict
+
 
 with virtual_experiment_onto:
     class HypothesisLattice:
@@ -17,12 +19,12 @@ with virtual_experiment_onto:
             if not self._is_correct():
                 raise Exception("Hypotheses not found in workflow")
 
-            transitive_closure = None
-            causal_mapping = None
+            transitive_closure = defaultdict(list)
 
             tasks = self.workflow.get_tasks()
             while (tasks):
                 current_task = tasks.get_current()
+                # remove current_task from tasks
                 remaining_tasks = self.workflow.get_remaining(current_task)
                 for h_i in current_task:
                     for t in remaining_tasks:
@@ -37,9 +39,33 @@ with virtual_experiment_onto:
             hyp_var_map = self._build_hypothesis_var_mapping(transitive_closure)
 
             for dep in hyp_var_map:
-                lattice.add_edge(hyp_var_map[0], hyp_var_map[1])
+                lattice.add_edge(dep[0], dep[1])
 
             return lattice
+
+        def add_hypothesis(self, hypothesis):
+            # check if all hypotheses are in workflow
+            if not self._is_correct():
+                raise Exception("Hypotheses not found in workflow")
+            tasks = self.workflow.get_tasks()
+            transitive_closure = defaultdict(list)
+
+            while (tasks):
+                current_task = tasks.get_current()
+                # remove current_task from tasks
+                for h_i in current_task:
+                    structure_i = h_i.structure
+                    united_str = structure_i.union(hypothesis.structure)
+                    if united_str.is_complete():
+                        tc = united_str.build_transitive_closure()
+                        transitive_closure.add(tc)
+
+            hyp_var_map = self._build_hypothesis_var_mapping(transitive_closure)
+
+            for dep in hyp_var_map:
+                self.lattice.add_edge(dep[0], dep[1])
+
+
         def derived_by(self, hypothesis):
             pass
 
@@ -54,11 +80,7 @@ with virtual_experiment_onto:
             pass
 
 
-        def add_hypothesis(self):
-            pass
-
-
-        def remove_hypothesis(self):
+        def remove_hypothesis(self, hypothesis):
             pass
 
         def _is_correct(self):
